f465405cb6883dff4f9bf4e95fcbad8f1c6e12a0
FIRSTMAYAGUI <firstmayaguijunior@gmail.com>
2026-09-21 22:28:06 +0200

    setup database connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import { pool } from './db/database.js';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +10,16 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.get('/test', async(req, res) => {
+  try {
+    const users = await pool.query('SELECT * FROM users');
+    res.json(users).status(200);
+  } catch (error) {
+    console.log(error)
+    res.json(error).status(500);
+  }
 });
 
 app.listen(PORT, () => {
